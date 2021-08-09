e4c082dbdaf05593a043394554d9154aa58b481f
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2021-08-09 16:15:10 +0900

    Disable dsn and use dsn by env

--- src/Sentry.ts
+++ src/Sentry.ts
@@ -1,8 +1,9 @@
 import * as Sentry from '@sentry/browser'
+import { sentryDSN } from './env';
 
 export function init(): void {
   Sentry.init({
-    dsn: "https://b0cc27b5b9c04726a4ec99ad36bef91a@o157493.ingest.sentry.io/1219537",
+    dsn: sentryDSN,
     beforeSend(event) {
       if (event.exception) {
         Sentry.showReportDialog({ eventId: event.event_id });
